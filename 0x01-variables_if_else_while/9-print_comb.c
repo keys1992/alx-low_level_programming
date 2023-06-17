8deda5f8dee977905a78ac5419b481347a5c0454
keys1992 <einsteinr25@gmail.com>
2023-06-16 21:55:24 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 100; i++)
 	{
 		putchar(i + '0');
 		{
-		if (i < 9)
+		if (i < 99)
 			putchar(',');
 		putchar(' ');
 		}
