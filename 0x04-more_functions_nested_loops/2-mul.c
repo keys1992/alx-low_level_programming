d86d5410f66979882f6450d6cad788035f4a5f46
keys1992 <einsteinr25@gmail.com>
2023-06-22 16:43:33 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - checks main code
+ * @a: take the first number
+ * @b: takes the second number
+ * Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+	int answer;
+
+	answer = a * b;
+	return (answer);
+}
