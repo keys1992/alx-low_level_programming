4ed316bc6592ff6aa6d55ddb9cca052b81e696dc
keys1992 <einsteinr25@gmail.com>
2023-06-22 19:45:53 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+
+	for (i = 0; i < 782849; i = i + 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
