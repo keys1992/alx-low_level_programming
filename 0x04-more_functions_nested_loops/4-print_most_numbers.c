52395b795cd62e416bc868f56ba3d33368d802ad
keys1992 <einsteinr25@gmail.com>
2023-06-22 17:26:42 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers 01356789
+ * print_most_numbers - print numbers 01356789
  * Return: void
  */
 void print_most_numbers(void)
@@ -11,7 +11,9 @@ void print_most_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
+		{
 			_putchar(c);
+		}
 	}
 	_putchar('\n');
 }
