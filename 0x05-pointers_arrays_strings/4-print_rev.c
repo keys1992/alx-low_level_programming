615ec5d04c4f854594ec279857ec853ed6effa43
keys1992 <einsteinr25@gmail.com>
2023-06-26 14:39:41 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev -  function that prints a string, in reverse
+ * followed by a new line
+ * @s: string
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
