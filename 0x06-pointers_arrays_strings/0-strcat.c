8e098277ae3b538a3caa235ffe8bfb2fd7049ebe
keys1992 <einsteinr25@gmail.com>
2023-06-29 15:05:35 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+/**
+ * *_strcat - function that concatenates two strings
+ * @dest: destination string pointer
+ * @src: source string pointer
+ * Return: pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int length_of_string, z;
+
+	length_of_string = 0;
+	while (dest[length_of_string] != '\0')
+	{
+		length_of_string++;
+	}
+	for (z = 0; src[z] != '\0'; z++; length_of_string++)
+	{
+		dest[length_of_strirng] = src[j];
+	}
+	dest[length_of_string] = '\0';
+	return (dest);
+}
